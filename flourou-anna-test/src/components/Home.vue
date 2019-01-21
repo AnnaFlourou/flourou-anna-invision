51767c484ef4411e11d4709efbfbdbe28c8bc261
AnnaFlourou <flourou.anna@gmail.com>
2019-01-21 13:13:44 +0100

    first commit

---
+++ flourou-anna-test/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img class="desktop_hero_bckd" src="@/assets/desktop_hero_background.jpg">
+    <Navbar></Navbar>
+    <h1>Mountains worthy of Tolkien's stories</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar"
+export default {
+  components: {
+    Navbar
+  },
+  name: 'home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.desktop_hero_bckd {
+position: absolute;
+height : 100vh;
+width: 100vw;
+}
+h1 {
+	color: #FFFFFF;
+}
+</style>
---
+++ flourou-anna-test/src/components/Navbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container-fluid">
+    <nav class="navbar row">
+      <img class="logo-white" src="@/assets/logo_white.svg">
+        <ul class="row nav_items">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Discover</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Hikes</a>
+            </li>
+        </ul>
+    </nav>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar {
+  height: 33px;
+  width: auto;
+}
+.logo-white {
+  margin-top: 6.8rem;
+  margin-left: 3rem;
+	height: 32px;
+	width: 163px;
+}
+li, a {
+  text-decoration: none;
+  list-style: none;
+	color: #FFFFFF;
+  font-weight: bold;
+}
+.nav_items {
+  margin-top: 6.8rem;
+  margin-right: 2rem;
+	height: 35px;
+  width: auto;
+	/* font-family: "SF Pro Display"; */
+	font-size: 17px;
+	font-weight: bold;
+	letter-spacing: 0.4px;
+}
+.nav-item {
+  margin-right: 2.2rem;
+}
+.active {
+  border-bottom: 1px solid #FFFFFF;
+}
+h1{
+	color: #FFFFFF;
+}
+</style>
